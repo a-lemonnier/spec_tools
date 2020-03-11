5e7a59dbd0e597603cfcfb27f368b5851c290023
a-lemonnier <audric.lemonnier@obspm.fr>
2020-03-11 22:06:49 +0100

    add a test

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@
 
 ./threshold -i selected -o spectra -t 1
 
+./trim -l 4700 -u 4800 -i data -o spectra_trimmed
+
 ./der_snr -d data -o all_snr.csv
 ./der_snr -d spectra -o selected_snr.csv
 
+cat selected_snr.csv
 
