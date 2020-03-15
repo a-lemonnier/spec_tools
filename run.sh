597e515d5ac8bee3bc1dd46e562bc50a5e901c31
a-lemonnier <audric.lemonnier@obspm.fr>
2020-03-15 14:22:04 +0100

    update test

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-./findncopy -i data -o selected -n star_list.txt -e sky
-
+./genrandspec
+./findncopy -i rand_spectra -o selected -n star_list.txt -e sky
 ./threshold -i selected -o spectra -t 1
-
-./trim -l 4700 -u 4800 -i data -o spectra_trimmed
-
-./der_snr -d data -o all_snr.csv
+./trim -l 4500 -u 5500 -i rand_spectra -o spectra_trimmed
+./der_snr -d rand_spectra -o all_snr.csv
 ./der_snr -d spectra -o selected_snr.csv
-
-
-./shift -h
+./shift -w 0.1 -i rand_spectra
 
